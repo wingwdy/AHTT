500a4cccda945800eac1d0cf728df37f1ef811cd
chenls <315554068@qq.com>
2026-04-28 08:59:31 +0800

    调整CP电压兼容模式12V的下限异常阈值

--- 02_App/Src/BSW/ComplexDeviceDriver/CP/Cdd_CPConfig.c
+++ 02_App/Src/BSW/ComplexDeviceDriver/CP/Cdd_CPConfig.c
@@ -63,7 +63,7 @@ const CddCPVolStateFilter_Struct c_stCddCPVolStateFilterGB[CDDCP_CFG_VOLSTATE_CO
     [eCddCPVolState_Ground] = {     0,      0,   2000,   2000,   CDDCP_CFG_GB_FILERCNT,      eCddCPVolState_Ground },
     [eCddCPVolState_6V] =     {  4000,   4200,   7800,   8000,   CDDCP_CFG_GB_FILERCNT,      eCddCPVolState_6V  },
     [eCddCPVolState_9V] =     {  7800,   8000,  10800,  11000,   CDDCP_CFG_GB_FILERCNT,      eCddCPVolState_9V  },
-    [eCddCPVolState_12V] =    { 11000,  11000,  13000,  13000,   CDDCP_CFG_GB_FILERCNT,      eCddCPVolState_12V },
+    [eCddCPVolState_12V] =    { 10800,  11000,  13000,  13000,   CDDCP_CFG_GB_FILERCNT,      eCddCPVolState_12V },
 };
 
 const CddCPVolStateFilter_Struct c_stCddCPVolStateFilterQB[CDDCP_CFG_VOLSTATE_COUNT] = 
@@ -71,7 +71,7 @@ const CddCPVolStateFilter_Struct c_stCddCPVolStateFilterQB[CDDCP_CFG_VOLSTATE_CO
     [eCddCPVolState_Ground] = {     0,      0,   2000,   2000,   CDDCP_CFG_QB_FILERCNT,      eCddCPVolState_Ground  },
     [eCddCPVolState_6V] =     {  3000,   3000,   7800,   8000,   CDDCP_CFG_QB_FILERCNT,      eCddCPVolState_6V  },
     [eCddCPVolState_9V] =     {  7800,   8000,  10800,  11000,   CDDCP_CFG_QB_FILERCNT,      eCddCPVolState_9V  },
-    [eCddCPVolState_12V] =    { 11000,  11000,  13000,  13000,   CDDCP_CFG_QB_FILERCNT,      eCddCPVolState_12V },
+    [eCddCPVolState_12V] =    { 10800,  11000,  13000,  13000,   CDDCP_CFG_QB_FILERCNT,      eCddCPVolState_12V },
 };
 
 /*******************************************************************************
